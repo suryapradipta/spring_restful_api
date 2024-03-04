267aecacd662b59ce67d2ce455777bd241645c04
I Nyoman Surya Pradipta <suryapradipta.my@gmail.com>
2024-03-04 16:26:18 +0800

    feat: i18in

--- src/main/java/com/ksatria/spring_restful_api/WebConfiguration.java
+++ src/main/java/com/ksatria/spring_restful_api/WebConfiguration.java
@@ -2,11 +2,19 @@ package com.ksatria.spring_restful_api;
 
 import com.ksatria.spring_restful_api.common.resolver.UserArgumentResolver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.List;
+import java.util.Locale;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
@@ -19,5 +27,19 @@ public class WebConfiguration implements WebMvcConfigurer {
         WebMvcConfigurer.super.addArgumentResolvers(resolvers);
 
         resolvers.add(userArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeChangeInterceptor);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver resolver = new AcceptHeaderLocaleResolver();
+        resolver.setDefaultLocale(Locale.US); // Set default locale
+        return resolver;
     }
 }
--- src/main/java/com/ksatria/spring_restful_api/service/ReportService.java
+++ src/main/java/com/ksatria/spring_restful_api/service/ReportService.java
@@ -4,24 +4,18 @@ import com.ksatria.spring_restful_api.beans.UserDetails;
 import com.ksatria.spring_restful_api.entity.Contact;
 import com.ksatria.spring_restful_api.entity.User;
 import com.ksatria.spring_restful_api.repository.ContactRepository;
-import com.ksatria.spring_restful_api.repository.UserRepository;
-import jakarta.transaction.TransactionScoped;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.util.JRLoader;
 import org.dataloader.DataLoader;
-import org.springframework.http.HttpHeaders;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 @AllArgsConstructor
@@ -30,45 +24,49 @@ public class ReportService {
     private final ContactRepository contactRepository;
     private final DataLoader<String, User> userLoader;
 
+
     @Transactional
     public ResponseEntity<byte[]> generateReport() {
         String jasperFilePath = "C:\\Users\\GTR6\\OneDrive - Ksatria Medical Systems Pty Ltd\\Documents\\GitHub\\spring_restful_api\\src\\main\\resources\\reports\\jasperDesigns\\ContactListReport.jasper";
 
         try {
+            // Load Jasper Report
             JasperReport jasperReport = (JasperReport) JRLoader.loadObjectFromFile(jasperFilePath);
 
+            // Create data sources
             List<Contact> contacts = contactRepository.getAllContacts();
             contacts.forEach(contact -> userLoader.load(contact.getUser().getName()));
 
             List<UserDetails> userDetailsList = new ArrayList<>();
-            for (Contact contact : contacts) {
-                String username = contact.getUser().getUsername();
-                userDetailsList.add(new UserDetails(username));
-            }
+            userDetailsList.add(new UserDetails(contacts.getFirst().getUser().getUsername()));
+
+
             JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(userDetailsList);
-
-
             JRBeanCollectionDataSource tableDataSource = new JRBeanCollectionDataSource(contacts);
 
-
+            //Set parameters
             Map<String, Object> parameters = new HashMap<>();
             parameters.put("TABLE_DATA_SOURCE", tableDataSource);
 
+            Locale locale = LocaleContextHolder.getLocale();
+            parameters.put(JRParameter.REPORT_RESOURCE_BUNDLE, getResourceBundle(locale));
+
+            // Fill report and generate JasperPrint
             JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
 
             byte[] pdfBytes = JasperExportManager.exportReportToPdf(jasperPrint);
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_PDF);
-            headers.setContentDispositionFormData("filename", "ContactListReport.pdf");
-
             JasperExportManager.exportReportToPdfFile(jasperPrint, "C:\\Users\\GTR6\\OneDrive - Ksatria Medical Systems Pty Ltd\\Documents\\GitHub\\spring_restful_api\\src\\main\\resources\\reports\\generatedReports\\ContactListReport.pdf");
 
-            return new ResponseEntity<>(pdfBytes, headers, HttpStatus.OK);
+            return new ResponseEntity<>(pdfBytes, HttpStatus.OK);
 
         } catch (JRException e) {
             e.printStackTrace();
             throw new RuntimeException("Error generating report", e);
         }
+    }
+
+    private ResourceBundle getResourceBundle(Locale locale) {
+        return ResourceBundle.getBundle("messages", locale);
     }
 }
